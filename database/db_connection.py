6bfd63ea3a9b77781454357356d756230d3875f7
Ytshak_Raiyner <yr3275744@gmail.com>
2026-06-11 16:45:36 +0300

    add crerat tabel functions

--- database/db_connection.py
+++ database/db_connection.py
@@ -8,17 +8,50 @@ def get_connection(user_name:str = "root", password:str = "library_manager", dat
                              port = port
                              )
 
-def create_books():
+def create_books_table():
     """docstring"""
     connection = get_connection()
     cursor = connection.cursor()
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS books(
-                   id PRIMARY_KEY AUTO_INCREMENT,
+                   id INT PRIMARY KEY AUTO_INCREMENT,
+                   title VARCHAR(50) NOT NULL,
+                   author VARCHAR(50) NOT NULL,
+                   genre ENUM('Fiction', 'Non-Fiction', 'Science', 'History', 'Other') NOT NULL,
+                   is_available BOOL NOT NULL,
+                   borrowed_by_member_id INT
+                   );""")
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return None
+
+def create_members_table():
+    """docstring"""
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS members(
+                   id INT PRIMARY KEY AUTO_INCREMENT,
+                   name VARCHAR(50) NOT NULL,
+                   email VARCHAR(255) UNIQUE NOT NULL,
+                   is_active BOOL NOT NULL,
+                   total_borrows INT NOT NULL
                    )""")
 
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return None
 
 if __name__ == "__main__":
-    conn = get_connection()
-    print("sss")
-    conn.close()
+    create_books_table()
+    create_members_table()
+    # conn = get_connection()
+    # cursor = conn.cursor()
+    # cursor.execute("DESCRIBE books")
+    # rows = cursor.fetchall()
+    # cursor.close()
+    # conn.close()
+    # print(rows)
