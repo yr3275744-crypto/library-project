e35daa6775c97e93f596fd1dada3bf4121b71ce0
Ytshak_Raiyner <yr3275744@gmail.com>
2026-06-11 14:48:20 +0300

    add main.py app

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-#TODO : create the files.
-# create simple router and server. 
+# TODO : (create the files.)
+# (create simple router and server.) 
 # create the conection to the database and tables.
 # daclarate the db classes.
 # daclarate the routs handle the methods in the db classes.
 # add logger and loggs.
+import uvicorn
+from fastapi import FastAPI
+from routes import book_routes, member_routes, report_routes
 
+app = FastAPI()
+
+app.include_router(book_routes.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app)
