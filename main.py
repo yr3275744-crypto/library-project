3a04f0200aa0d34d2d1f3e43b00b90650d7923c3
Ytshak_Raiyner <yr3275744@gmail.com>
2026-06-11 11:47:39 +0300

    add the files

---
+++ main.py
@@ -0,0 +1,7 @@
+#TODO : create the files.
+# create simple router and server. 
+# create the conection to the database and tables.
+# daclarate the db classes.
+# daclarate the routs handle the methods in the db classes.
+# add logger and loggs.
+
